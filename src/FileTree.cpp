7b8551823cc749883e0aa9dec35a31e1c1a594b7
lintopher0315 <lin978@purdue.edu>
2021-01-05 18:17:36 -0500

    filetree items open corresponding editor when activated

--- src/FileTree.cpp
+++ src/FileTree.cpp
@@ -7,16 +7,29 @@ FileTree::FileTree(wxWindow *parent) : wxTreeCtrl(parent, wxID_ANY, wxDefaultPos
 	cwd = getFrame()->cwd;
 	root = AddRoot(cwd);
 	loadTree(cwd, root);
+
+	Bind(wxEVT_TREE_ITEM_ACTIVATED, &FileTree::onActivate, this);
 }
 
 Frame *FileTree::getFrame() const {
 	return (Frame *)GetParent();
 }
 
+void FileTree::onActivate(wxTreeEvent& event) {
+	std::string relPath = getRelPathFromItem(event.GetItem());
+	std::string absPath = cwd + relPath;
+	if (std::filesystem::is_regular_file(absPath)) {
+		Window *w = getFrame()->window;
+		w->panel->AddPage(new Editor(w->panel), relPath, true);
+		w->getCurrentEditor()->relPath = relPath;
+		w->getCurrentEditor()->LoadFile(absPath);
+	}
+}
+
 void FileTree::loadTree(const std::string& cwd, wxTreeItemId parent) {
 	for (const auto & file : std::filesystem::directory_iterator(cwd)) {
 		wxTreeItemId item = AppendItem(parent, file.path().filename().string());
-		if (is_directory(file.path())) {
+		if (std::filesystem::is_directory(file.path())) {
 			loadTree(file.path().string(), item);	
 		}
 	}
@@ -26,4 +39,17 @@ void FileTree::loadTree(const std::string& cwd, wxTreeItemId parent) {
 void FileTree::reloadTree() {
 	DeleteChildren(root);
 	loadTree(cwd, root);
+}
+
+std::string FileTree::getRelPathFromItem(const wxTreeItemId& item) const {
+	std::string absPath;
+	wxTreeItemId id = item;
+	while (id != root) {
+		absPath.insert(0, GetItemText(id).ToStdString() + "/");
+		id = GetItemParent(id);
+	}
+	if (!absPath.empty()) {
+		absPath.pop_back();
+	}
+	return absPath;
 }
--- src/FileTree.h
+++ src/FileTree.h
@@ -13,8 +13,10 @@ class FileTree : public wxTreeCtrl {
 		FileTree(wxWindow *parent);
 
 		Frame *getFrame(void) const;
+		void onActivate(wxTreeEvent& event);
 		void loadTree(const std::string& cwd, wxTreeItemId parent);
 		void reloadTree(void);
+		std::string getRelPathFromItem(const wxTreeItemId& item) const;
 
 		std::string cwd;
 		wxTreeItemId root;
