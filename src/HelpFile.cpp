c70bc641aa928db10622fd261fdb5605f4ee5e68
lintopher0315 <lin978@purdue.edu>
2021-03-12 01:35:38 -0500

    added styling to help file

--- src/HelpFile.cpp
+++ src/HelpFile.cpp
@@ -2,13 +2,27 @@
 #include "FileHelper.h"
 
 HelpFile::HelpFile(wxWindow *parent) : Editor(parent, NULL_LEX) {
+	SetLexer(wxSTC_LEX_LUA);
+
+	StyleSetForeground(wxSTC_LUA_WORD4, wxColor(209, 73, 94));
+	StyleSetForeground(wxSTC_LUA_WORD5, wxColor(219, 131, 0));
+	StyleSetForeground(wxSTC_LUA_WORD6, wxColor(120, 161, 109));
+	StyleSetForeground(wxSTC_LUA_WORD7, wxColor(147, 196, 82));
+	StyleSetForeground(wxSTC_LUA_WORD8, wxColor(222, 149, 222));
+
+	SetKeyWords(3, wxT("Gadget"));
+	SetKeyWords(4, wxT("NORMAL"));
+	SetKeyWords(5, wxT("EDIT"));
+	SetKeyWords(6, wxT("VISUAL"));
+	SetKeyWords(7, wxT("LINE"));
+
 	AddText(
 		"Welcome to the Gadget help file!\n"
 		"This file is read-only\n\n"
 
 		"Gadget is a GUI text editor inspired by GVim, the interface variant of the popular text editor, Vim.\n\n"
 
-		"Gadget features four modes: NORMAL, EDIT, VISUAL, and LINE.\n\n"
+		"Gadget features four modes: NORMAL EDIT VISUAL and LINE\n\n"
 
 		"~~~~~~~~~~~~\nNORMAL MODE:\n~~~~~~~~~~~~\n\n"
 
@@ -17,7 +31,7 @@ HelpFile::HelpFile(wxWindow *parent) : Editor(parent, NULL_LEX) {
 		"b\n\t\t\t\tjump backward to the start of the previous word; can be prepended with [num]\n"
 		"gg\n\t\t\t\tjump to the beginning of the file\n"
 		"G\n\t\t\t\tjump to the end of the file\n"
-		"[num]G\n\t\t\t\tjump to the beginning of the line indicated by [num]\n"
+		"<num>G\n\t\t\t\tjump to the beginning of the line indicated by [num]\n"
 		"_\n\t\t\t\tjump to the first non-whitespace character of the current line\n"
 		"0\n\t\t\t\tjump to the beginning of the current line\n"
 		"$\n\t\t\t\tjump to the end of the current line\n"
@@ -33,10 +47,10 @@ HelpFile::HelpFile(wxWindow *parent) : Editor(parent, NULL_LEX) {
 		"p\n\t\t\t\tpaste the contents of the clipboard after the caret\n"
 		"P\n\t\t\t\tpaste the contents of the clipboard before the caret\n"
 		"[num]gt\n\t\t\t\tswitch to the tab indicated by [num]\n"
-		"i\n\t\t\t\tswitch to INSERT mode\n"
-		"I\n\t\t\t\tswitch to INSERT mode and jump to the first non-whitespace character of the current line\n"
-		"a\n\t\t\t\tswitch to INSERT mode and move caret right once\n"
-		"A\n\t\t\t\tswitch to INSERT mode and jump to the end of the current line\n"
+		"i\n\t\t\t\tswitch to EDIT mode\n"
+		"I\n\t\t\t\tswitch to EDIT mode and jump to the first non-whitespace character of the current line\n"
+		"a\n\t\t\t\tswitch to EDIT mode and move caret right once\n"
+		"A\n\t\t\t\tswitch to EDIT mode and jump to the end of the current line\n"
 		"v\n\t\t\t\tswitch to VISUAL mode\n"
 		"V\n\t\t\t\tswitch to LINE mode\n"
 		":q\n\t\t\t\tquit and close the buffer\n"
